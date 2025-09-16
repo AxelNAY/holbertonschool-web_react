c07f77f11c0aa5b5f92aa24448a05462e11fed53
AxelNAY <axelnay@sfr.fr>
2025-09-16 14:30:58 +0200

    Complete task 1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="start-btn">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    let click_count = 0;
+
+    function updateCounter() {
+        click_count += 1;
+        $('#count').text(`${click_count} clicks on the button`);
+    }
+
+    $('#count').text(`${click_count} clicks on the button`);
+
+    $('#start-btn').on('click', _.debounce(updateCounter, 500));
+});
