40a58490077f7ac2e66023bdd5704adde231c772
AxelNAY <axelnay@sfr.fr>
2025-09-16 10:50:08 +0200

    Complete task 0

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+// Import jQuery using ES6 import syntax
+import $ from 'jquery';
+
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+});
