274cdc2bc8930426badb298d83c3fdcea84aaddd
AxelNAY <axelnay@sfr.fr>
2025-09-17 11:11:43 +0200

    Update webpack.config.js

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -25,6 +25,27 @@ module.exports = {
               outputPath: 'images',
             },
           },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          },
         ],
       },
     ],
