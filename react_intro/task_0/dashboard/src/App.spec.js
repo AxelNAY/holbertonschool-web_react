7552d0aea8197284d78997fd948265317f278eb4
AxelNAY <axelnay@sfr.fr>
2025-09-23 11:37:08 +0200

    Update App.spec.js

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -14,16 +14,13 @@ test('Verify if the content of the two paragraph', () => {
   render(<App />)
   const BodyParaph = screen.getByText('Login to access the full dashboard');
   expect(BodyParaph).toBeInTheDocument();
-  expect(BodyParaph.tagName.toLowerCase()).toBe('p');
 
   const FooterParaph = screen.getByText('Copyright 2025 - holberton School');
   expect(FooterParaph).toBeInTheDocument();
-  expect(BodyParaph.tagName.toLowerCase()).toBe('p');
 })
 
 test('Verify if the image element is rendered', () => {
   render(<App />)
   const Image = screen.getByAltText(/holberton logo/i);
   expect(Image).toBeInTheDocument();
-  expect(Image.tagName.toLowerCase()).toBe('img');
 })
