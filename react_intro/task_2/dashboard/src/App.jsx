446e080737ac85ce38372cfc66c669b512202aff
AxelNAY <axelnay@sfr.fr>
2025-09-23 15:49:33 +0200

    Update App.jsx

---
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+import holbertonLogo from './assets/holberton-logo.jpg'
+import { getCurrentYear, getFooterCopy } from './utils.js'
+import Notifications from './Notifications.jsx'
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div class="App-header">
+        <img src={holbertonLogo} alt="holberton logo" className="App-logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div class="App-body">
+        <p>Login to access the full dashboard</p>
+        <label id="email">
+          <input placeholder='email'>
+          </input>
+        </label>
+        <label id="password">
+          <input placeholder='password'>
+          </input>
+        </label>
+        <button>OK</button>
+      </div>
+      <div class="App-footer">
+        <p>Copyright { getCurrentYear() } - { getFooterCopy(true) }</p>
+      </div>
+    </>
+  )
+}
+
+export default App
