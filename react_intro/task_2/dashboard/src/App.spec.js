797854d8b56eea15d41087b5671de1d77ab83602
AxelNAY <axelnay@sfr.fr>
2025-09-24 10:36:05 +0200

    Update App.spec.jsx

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -3,48 +3,40 @@ import App from './App.jsx'
 
 test('Verify if h1 contains School dashboard', () => {
   render(<App />)
-  const h1Element = screen.getByRole('heading', { 
-    level: 1, 
-    name: /school dashboard/i 
-  });
-  expect(h1Element).toBeInTheDocument();
+  expect(
+    screen.getByRole('heading', { level: 1, name: /school dashboard/i })
+  ).toBeInTheDocument()
 })
 
 test('Verify if the content of the two paragraphs is correct', () => {
   render(<App />)
-  const bodyParagraph = screen.getByText(/login to access the full dashboard/i);
-  expect(bodyParagraph).toBeInTheDocument();
+  expect(
+    screen.getByText(/login to access the full dashboard/i)
+  ).toBeInTheDocument()
 
-  const footerParagraph = screen.getByText(/copyright.*holberton.*school/i);
-  expect(footerParagraph).toBeInTheDocument();
+  expect(
+    screen.getByText(/copyright.*holberton.*school/i)
+  ).toBeInTheDocument()
 })
 
 test('Verify if the image element is rendered', () => {
   render(<App />)
-  const imgElement = screen.getByAltText(/holberton logo/i);
-  expect(imgElement).toBeInTheDocument();
+  expect(screen.getByAltText(/holberton logo/i)).toBeInTheDocument()
 })
 
 test('Verify if the 2 input elements are rendered', () => {
   render(<App />)
-  const emailInput = screen.getByPlaceholderText(/email/i);
-  expect(emailInput).toBeInTheDocument();
-
-  const passwordInput = screen.getByPlaceholderText(/password/i);
-  expect(passwordInput).toBeInTheDocument();
+  expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+  expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
 })
 
 test('Verify if the 2 label elements are rendered', () => {
   render(<App />)
-  const emailLabel = screen.getByText(/email/i);
-  expect(emailLabel).toBeInTheDocument();
-
-  const passwordLabel = screen.getByText(/password/i);
-  expect(passwordLabel).toBeInTheDocument();
+  expect(screen.getByText(/email/i)).toBeInTheDocument()
+  expect(screen.getByText(/password/i)).toBeInTheDocument()
 })
 
 test('Verify if the button element is rendered', () => {
   render(<App />)
-  const okButton = screen.getByRole('button', { name: /ok/i });
-  expect(okButton).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /ok/i })).toBeInTheDocument()
 })
