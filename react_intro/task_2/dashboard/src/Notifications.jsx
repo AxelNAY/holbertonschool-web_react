bc9984547659321fce83a12cc1ac5c740d1bc82a
AxelNAY <axelnay@sfr.fr>
2025-09-24 15:53:10 +0200

    Update Notifications.jsx

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -6,12 +6,12 @@ function Notifications() {
   return (
     <div class="notification-items">
       <p>Here is the list of notifications</p>
-      <button aria-label="Close"><img src={Close}/></button>
       <ul>
         <li data-priority>New course available</li>
         <li data-priority>New resume available</li>
         <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
       </ul>
+      <button aria-label="Close"><img src={Close}/></button>
     </div>
   )
 }
