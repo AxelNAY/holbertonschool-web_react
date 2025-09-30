e49712077aa4c64ea2c0df21fd75460f95f7f46a
AxelNAY <axelnay@sfr.fr>
2025-09-30 15:23:56 +0200

    Complete structure

--- react_props/task_1/dashboard/src/App/App.jsx
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -1,34 +1,20 @@
+import { Fragment } from 'react'
 import './App.css'
-import holbertonLogo from '../assets/holberton-logo.jpg'
-import { getCurrentYear, getFooterCopy } from '../utils/utils.js'
 import Notifications from '../Notifications/Notifications.jsx'
+import Header from '../Header/Header.jsx'
+import Login from '../Login/Login.jsx'
+import Footer from '../Footer/Footer.jsx'
 
 function App() {
   return (
-    <>
+    <Fragment>
       <div className="root-notifications">
         <Notifications />
       </div>
-      <div class="App-header">
-        <img src={holbertonLogo} alt="holberton logo" className="App-logo" />
-        <h1>School dashboard</h1>
-      </div>
-      <div class="App-body">
-        <p>Login to access the full dashboard</p>
-        <label id="email">Email: 
-          <input placeholder='email'>
-          </input>
-        </label>
-        <label id="password">Password: 
-          <input placeholder='password'>
-          </input>
-        </label>
-        <button>OK</button>
-      </div>
-      <div class="App-footer">
-        <p>Copyright { getCurrentYear() } - { getFooterCopy(true) }</p>
-      </div>
-    </>
+      <Header />
+      <Login />
+      <Footer />
+    </Fragment>
   )
 }
 
--- react_props/task_1/dashboard/src/App/App.spec.js
+++ react_props/task_1/dashboard/src/App/App.spec.js
@@ -1,42 +1,10 @@
 import { render, screen } from '@testing-library/react'
-import App from './App.jsx'
+import App from './App'
 
-test('Verify if h1 contains School dashboard', () => {
+test('Verify if all components are rendered', () => {
   render(<App />)
-  expect(
-    screen.getByRole('heading', { level: 1, name: /school dashboard/i })
-  ).toBeInTheDocument()
-})
-
-test('Verify if the content of the two paragraphs is correct', () => {
-  render(<App />)
-  expect(
-    screen.getByText(/login to access the full dashboard/i)
-  ).toBeInTheDocument()
-
-  expect(
-    screen.getByText(/copyright.*holberton.*school/i)
-  ).toBeInTheDocument()
-})
-
-test('Verify if the image element is rendered', () => {
-  render(<App />)
-  expect(screen.getByAltText(/holberton logo/i)).toBeInTheDocument()
-})
-
-test('Verify if the 2 input elements are rendered', () => {
-  render(<App />)
-  expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
-  expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
-})
-
-test('Verify if the 2 label elements are rendered', () => {
-  render(<App />)
-  expect(screen.getByText(/email/i)).toBeInTheDocument()
-  expect(screen.getByText(/password/i)).toBeInTheDocument()
-})
-
-test('Verify if the button element is rendered', () => {
-  render(<App />)
-  expect(screen.getByRole('button', { name: /ok/i })).toBeInTheDocument()
+  expect(screen.getByText(/here is the list of notifications/i)).toBeInTheDocument()
+  expect(screen.getByRole('heading', { level: 1, name: /school dashboard/i })).toBeInTheDocument()
+  expect(screen.getByText(/login to access the full dashboard/i)).toBeInTheDocument()
+  expect(screen.getByText(/copyright.*holberton.*school/i)).toBeInTheDocument()
 })
