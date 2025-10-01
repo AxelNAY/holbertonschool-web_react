4a201ec62baadba9d3e6a16b7e1115b593280c6d
AxelNAY <axelnay@sfr.fr>
2025-10-01 11:26:23 +0200

    Add code in App.css

---
+++ react_intro/task_5/dashboard/src /App/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import holbertonLogo from '../assets/holberton-logo.jpg'
+import { getCurrentYear, getFooterCopy } from '../utils/utils.js'
+import Notifications from '../Notifications/Notifications.jsx'
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div class="App-header">
+        <img src={holbertonLogo} alt="holberton logo" className="App-logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div class="App-body">
+        <p>Login to access the full dashboard</p>
+        <label id="email">Email: 
+          <input placeholder='email'>
+          </input>
+        </label>
+        <label id="password">Password: 
+          <input placeholder='password'>
+          </input>
+        </label>
+        <button>OK</button>
+      </div>
+      <div class="App-footer">
+        <p>Copyright { getCurrentYear() } - { getFooterCopy(true) }</p>
+      </div>
+    </>
+  )
+}
+
+export default App
