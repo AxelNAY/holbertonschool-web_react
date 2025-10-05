167513c87d252d2874ed4d05ec45d09ef93af04d
AxelNAY <axelnay@sfr.fr>
2025-10-05 23:57:30 +0200

    Complete

---
+++ react_props/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,21 @@
+function NotificationItem({ type = 'default', html, value }) {
+  const style = type === 'urgent' ? { color: 'red' } : { color: 'blue' };
+
+  if (html) {
+    return (
+      <li
+        data-notification-type={type}
+        style={style}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    );
+  }
+
+  return (
+    <li data-notification-type={type} style={style}>
+      {value}
+    </li>
+  );
+}
+
+export default NotificationItem;
