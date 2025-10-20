22a1a10fcf589231922c76c9a651f59fc656ce64
AxelNAY <axelnay@sfr.fr>
2025-10-20 15:39:26 +0200

    Creation of react_styling folder

---
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/App.jsx';
+import './main.css';
+
+/* Import Roboto font weights */
+import '@fontsource/roboto/400.css'; // Regular
+import '@fontsource/roboto/500.css'; // Medium
+import '@fontsource/roboto/700.css'; // Bold
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
