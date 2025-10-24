54de94f2882c633fc1f6bf4de748dd03d259ed52
AxelNAY <axelnay@sfr.fr>
2025-10-24 15:02:31 +0200

    Fix tailwind integration

--- react_styling/task_0/dashboard/src/main.jsx
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/App.jsx';
 
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import './main.css';
+import App from './App/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
