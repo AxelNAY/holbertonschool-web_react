df1de507e233bef7b357e50629949d60a82066c4
AxelNAY <axelnay@sfr.fr>
2025-11-05 10:31:31 +0100

    Duplicate task_1/dashboard in task_2 and modify main.css and Notifications folder

---
+++ react_styling/task_2/dashboard/src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css';
+import holbertonLogo from '../assets/holberton-logo.jpg'; // ou .jpg
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <img
+        className="App-logo"
+        src={holbertonLogo}
+        alt="Holberton logo"
+      />
+      <h1>School dashboard</h1>
+    </header>
+  );
+}
