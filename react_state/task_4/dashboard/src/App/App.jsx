232f4662ddb556a405926414fa0708430700935d
AxelNAY <axelnay@sfr.fr>
2026-03-06 10:02:04 +0100

    Fixing task_4

--- react_state/task_4/dashboard/src/App/App.jsx
+++ react_state/task_4/dashboard/src/App/App.jsx
@@ -87,9 +87,9 @@ class App extends Component {
 
   markNotificationAsRead = (id) => {
     console.log(`Notification ${id} has been marked as read`);
-    this.setState((prevState) => ({
-      notifications: prevState.notifications.filter((n) => n.id !== id),
-    }));
+    this.setState({
+      notifications: this.state.notifications.filter(notif => notif.id !== id)
+    });
   };
 
   render() {
